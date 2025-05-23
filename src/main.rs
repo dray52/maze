ec73bee7101e4881a7f70a9dd28aa904e37d2110
dray52 <levesquedraydon@gmail.com>
2025-05-23 15:18:33 -0400

    maze game

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+/*
+By: <Your Name Here>
+Date: 2025-05-16
+Program Details: <Program Description Here>
+*/
+
+mod modules;
+
+use crate::modules::collision::check_collision;
+ use crate::modules::preload_image::TextureManager;
+use crate::modules::preload_image::LoadingScreenOptions;
+use crate::modules::still_image::StillImage;
+use macroquad::prelude::*;
+/// Set up window settings before the app runs
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "maze".to_string(),
+        window_width: 1024,
+        window_height: 768,
+        fullscreen: false,
+        high_dpi: true,
+        window_resizable: true,
+        sample_count: 4, // MSAA: makes shapes look smoother
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+     let loading_options = LoadingScreenOptions {
+        title: Some("MAZE".to_string()),
+        background_color: BLACK,
+        bar_fill_color: Color::new(1.0, 0.0, 0.0, 1.0), // Brighter green
+        // Use default values for other options
+        ..Default::default()
+    };
+    let maze = StillImage::new("assets/maze.png", 500.0, 500.0, 200.0, 60.0, true, 1.0).await;
+    let mut character = StillImage::new("assets/amongus.png", 35.0, 35.0, 200.0, 20.0, true, 1.0).await;
+
+let tm = TextureManager::new();
+let all_assets = ["assets/maze.png", "assets/amongus.png"];
+
+  tm.preload_with_loading_screen(&all_assets, Some(loading_options)).await;
+    let speed = 100.0;
+
+
+
+    loop {
+         let old_spot=character.pos();
+        clear_background(GREEN);
+        if is_key_down(KeyCode::Right) {
+            character.set_position(vec2(character.pos().x + speed*get_frame_time(), character.pos().y));
+        }
+         if is_key_down(KeyCode::Left) {
+            character.set_position(vec2(character.pos().x - speed*get_frame_time(), character.pos().y));
+        }
+         if is_key_down(KeyCode::Down) {
+            character.set_position(vec2(character.pos().x, character.pos().y+speed*get_frame_time()));
+        }
+         if is_key_down(KeyCode::Up) {
+            character.set_position(vec2(character.pos().x, character.pos().y-speed*get_frame_time()));
+        }let collision = check_collision(&character, &maze, 1);
+
+        if collision {
+            character.set_position(old_spot);
+        }
+        character.draw();
+maze.draw();
+        next_frame().await;
+    }
+}
